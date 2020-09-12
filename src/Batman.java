f47b50320a7e8314f3ee1f8548d69ed0abc1d756
Rory McFeely <rory@imac.lan>
2020-09-12 15:26:23 +0100

    at it batman

---
+++ src/Batman.java
@@ -0,0 +1,14 @@
+public class Batman {
+    public static void main (String[] Args){
+        System.out.println("I am the night");
+        if (true) {
+            System.out.println("I am vengeance");
+
+        }
+        for (int i=0; i<10; i++){
+            System.out.println(i);
+
+        }
+    }
+
+}
